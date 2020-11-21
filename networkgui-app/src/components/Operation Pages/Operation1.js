a94e65f93e42afcae31a93d948fa27a41d5f0878
E-Jon Thomas <et604127@digicelgroup.com>
2020-11-21 18:30:43 -0500

    updating folder structure

--- networkgui-app/src/components/Operation Pages/Operation1.js
+++ networkgui-app/src/components/Operation Pages/Operation1.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {Row, Col} from 'antd';
 import 'antd/dist/antd.css';
 import '../../cssComponents/operations.css'
-import ActivityTable from './Sub Op Components/ActivityTable'
-import {Card} from 'antd';
+
+import Operation from './Sub Op Components/Operation';
 
 
 
@@ -11,21 +10,7 @@ class Operation1 extends React.Component{
     render(){
         return(
             <div>
-                <Card title="Associated Attachments">
-                    <Row>
-                        <Col flex={5}>                        
-                                                    
-                        </Col>
-                    </Row>    
-                </Card>
-
-                <Card title="Acitivty Log">                  
-                    <Row>
-                        <Col flex={5}>                        
-                            <ActivityTable/>                        
-                        </Col>
-                    </Row>   
-                </Card>
+                <Operation/>
             </div>
         );
     }
---
+++ networkgui-app/src/components/Operation Pages/Sub Op Components/Operation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+import 'antd/dist/antd.css';
+import '../../cssComponents/operations.css'
+import ActivityTable from './Sub Op Components/ActivityTable'
+import {Card} from 'antd';
+
+
+
+class Operation extends React.Component{
+    render(){
+        return(
+            <div>
+                <Card title="Associated Attachments">
+                    <Row>
+                        <Col flex={5}>                        
+                                                    
+                        </Col>
+                    </Row>    
+                </Card>
+
+                <Card title="Acitivty Log">                  
+                    <Row>
+                        <Col flex={5}>                        
+                            <ActivityTable/>                        
+                        </Col>
+                    </Row>   
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default Operation;
